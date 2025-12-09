916f248a24fa6f73adf59fa55072db6255e377d6
Son Luu <son.lt1103@gmail.com>
2025-12-09 13:04:59 +0900

    update test and dev set

---
+++ eval.sh
@@ -0,0 +1,4 @@
+python src/F1_scorer.py \
+    --golddir="/home/sonlt/jaist_drive/sentiment_graphs/data/sent_graphs/vietnam/" \
+    --preddir="/home/sonlt/jaist_drive/sentiment_graphs/experiments/vietnam/bartpho" \
+    --istest \
