61e953110c863c9015cb0f2cc0378ade2d327f7b
Son Luu <son.lt1103@gmail.com>
2025-12-21 19:20:49 +0900

    add confusion matrix script

---
+++ confusion_eval.sh
@@ -0,0 +1,5 @@
+path="head_final-inside_label"
+python src/confusion.py \
+    /home/sonlt/drive/sentiment_graphs/data/sent_graphs/vietnam/$path/train.conllu  \
+    /home/sonlt/drive/sentiment_graphs/data/sent_graphs/vietnam/$path/dev.conllu  \
+    /home/sonlt/drive/sentiment_graphs/experiments/vietnam/caffebert/$path/1/dev.conllu.pred
